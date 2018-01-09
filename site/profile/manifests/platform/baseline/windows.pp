3b4ab14a31f5519a8a9fef87682d04b7c34f7463
puppet <puppet@localhost.local>
2018-01-09 20:26:05 +0000

    Update 'site/profile/manifests/platform/baseline/windows.pp'

--- site/profile/manifests/platform/baseline/windows.pp
+++ site/profile/manifests/platform/baseline/windows.pp
@@ -1,10 +1,10 @@
 class profile::platform::baseline::windows {
-
   include ::profile::platform::baseline::windows::bootstrap
-  include ::profile::platform::baseline::windows::common
+#  include ::profile::platform::baseline::windows::common
   include ::profile::platform::baseline::windows::motd
   include ::profile::platform::baseline::windows::firewall
   include ::profile::platform::baseline::windows::packages
   include ::profile::platform::baseline::users::windows
-
+# Added for security demo
+  include ::profile::platform::baseline::windows::security_baseline
 }

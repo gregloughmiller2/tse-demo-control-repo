ee3a45aa3212ca897c718074d1e5d83cd7a0f040
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-19 09:12:58 -0500

    wsus client

---
+++ site/example/manifests/greg_demo_wsus_client.pp
@@ -0,0 +1,9 @@
+class example::greg_demo_wsus_client {
+
+class { '::wsus_client':
+    server_url                     => 'http://10.2.1.17:8530',
+    auto_update_option             => 'NotifyOnly',
+    detection_frequency_hours      => '1',
+    enable_status_server           => 'true',
+    }
+}

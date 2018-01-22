0152ff0a19350af3f92f090c2635f2619a4120de
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-22 14:35:00 -0500

    windows dsc demo

---
+++ site/example/manifests/greg_demo_win_dsc.pp
@@ -0,0 +1,13 @@
+class example::greg_demo_win_dsc {
+
+dsc_user { 'demouser':
+  dsc_username   => 'demouser',
+  dsc_Description  => 'demo',
+  dsc_ensure       => present,
+  dsc_password     => {
+    'user'  => 'demouser',
+    'password' => Sensitive('demopassword')
+    },
+  dsc_passwordneverexpiers  => false,
+  }
+}

739c47f7db9e311d13dea11fdd30bac03cc5e456
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-22 12:54:54 -0500

    change wsus client options

--- site/example/manifests/greg_demo_wsus_client.pp
+++ site/example/manifests/greg_demo_wsus_client.pp
@@ -2,7 +2,7 @@ class example::greg_demo_wsus_client {
 
 class { '::wsus_client':
     server_url                     => 'http://10.2.1.17:8530',
-    auto_update_option             => 'NotifyOnly',
+    auto_update_option             => 'AutoInstall',
     detection_frequency_hours      => '1',
     enable_status_server           => true,
     }

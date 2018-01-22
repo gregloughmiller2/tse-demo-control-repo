6700160a65a2ef3eaa18100e946dd32a8c87cdcc
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-22 13:24:45 -0500

    windows features demo

--- site/example/manifests/greg_demo_win_features.pp
+++ site/example/manifests/greg_demo_win_features.pp
@@ -1,6 +1,6 @@
 class example::greg_demo_win_features {
 
-$demo_features = [ 'Web-Asp-Net45','NET-Framework-45-ASPNET','Windows Defender']
+$demo_features = [ 'NET-Framework-Core','Windows Defender']
 
 windowsfeature { $demo_features:
   ensure => present,

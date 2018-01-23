e8ac7650e292ce705380ff1050b5f5576aea54c4
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-23 15:06:05 -0500

    add registry test to dsc

--- site/example/manifests/greg_demo_win_dsc.pp
+++ site/example/manifests/greg_demo_win_dsc.pp
@@ -4,4 +4,11 @@ dsc_windowsfeature { 'IIS':
   dsc_ensure       => present,
   dsc_name  => 'Web-Server',
   }
+
+dsc_registry {'registry_demo':
+  dsc_ensure 	=> present,
+  dsc_key	=> 'HKEY_LOCAL_MACHINE\SOFTWARE\DemoKey',
+  dsc_valuename	=> 'DSCPuppet',
+  dsc_valuedata	=> 'This is a test with DSc Puppet Module',
+  } 
 }

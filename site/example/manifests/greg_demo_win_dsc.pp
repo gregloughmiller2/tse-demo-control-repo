c1390163ec0372df3404b1a42a25f74b94dd58ea
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-23 14:42:58 -0500

    add admin user to dsc pp

--- site/example/manifests/greg_demo_win_dsc.pp
+++ site/example/manifests/greg_demo_win_dsc.pp
@@ -1,23 +1,7 @@
 class example::greg_demo_win_dsc {
 #
-dsc_user { 'demouser':
-  dsc_username   => 'demouser',
-  dsc_description  => 'demo',
+dsc_windowsfeature { 'IIS':
   dsc_ensure       => present,
-  dsc_password	=>	{
-    'user'   =>    'demouser',
-    'password'    =>    'mysecretpassword12!'
-  }, 
-  dsc_passwordneverexpires  => false,
-  dsc_passwordchangerequired  => true,
-  dsc_disabled  => false,
-  }
-
-user { 'demoadmin':
-  name		=>	'demoadmin',
-  ensure	=> 	present,
-  groups	=>	['Administrators'],
-  membership	=>	inclusive,
-  password	=>	'secretpassword',
+  dsc_name  => 'Web-Server',
   }
 }

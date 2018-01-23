baf654da4010a7516f77e7a23b693c7ea3e2e36d
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-23 14:23:13 -0500

    add admin user to dsc pp

--- site/example/manifests/greg_demo_win_dsc.pp
+++ site/example/manifests/greg_demo_win_dsc.pp
@@ -6,7 +6,7 @@ dsc_user { 'demouser':
   dsc_ensure       => present,
   dsc_password     =>  {
     'user' => 'demouser',
-    'password => 'demopassword'
+    'password => Sensitive('demopassworda)'
     },
   dsc_passwordneverexpires  => false,
   }

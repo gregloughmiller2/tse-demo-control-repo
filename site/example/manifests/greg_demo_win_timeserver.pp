3a7496cf44736cf7ba7a6f0c34c5eeab2060849b
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-23 15:19:28 -0500

    syntax for time server

--- site/example/manifests/greg_demo_win_timeserver.pp
+++ site/example/manifests/greg_demo_win_timeserver.pp
@@ -1,11 +1,8 @@
 class example::greg_demo_win_timeserver  {
 
-###  include ::windowstime
-
   class { 'windowstime':
     servers  => { 'pool.ntp.org'     => '0x01',
                   'time.windows.com' => '0x01',
-                  'time.nist.gov'    => '0x02',
                 },
     timezone => 'Eastern Standard Time',
   }

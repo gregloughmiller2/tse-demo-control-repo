b796c1f5cd2865b183f4e2532a427c1355c1849b
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-24 13:33:57 -0500

    correct syntax on file manipulation

--- site/example/manifests/greg_demo_win_file.pp
+++ site/example/manifests/greg_demo_win_file.pp
@@ -3,11 +3,6 @@ class example::greg_demo_win_file {
 file { 'c:\\Temp\thisisdemo.conf':
    ensure 	=> present,
    content	=> 'This is a demo configuration file '
-}
-
-file { 'c:\\Temp\demofilefromsource.config':
-   ensure	=> present,
-   source       => 'puppet:///modules/example/demofilefromsource.config',
    }
 
 }

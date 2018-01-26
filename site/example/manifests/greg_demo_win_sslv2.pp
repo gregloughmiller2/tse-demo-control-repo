19203a7fd5c261e3c96509832ec56d9843b85082
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-26 09:21:10 -0500

    ssl keys

--- site/example/manifests/greg_demo_win_sslv2.pp
+++ site/example/manifests/greg_demo_win_sslv2.pp
@@ -7,7 +7,8 @@ class example::greg_demo_win_sslv2 {
   }
 
   registry_key { 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server\Enabled':
-    ensure       => present,
+    type       => dword,
+    data       => '0x1',
   }
 
 }

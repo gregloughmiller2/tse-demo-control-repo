252e00b184f4993a78357911dee9f60c5738441c
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-26 09:55:08 -0500

    ssl stuff

--- site/example/manifests/greg_demo_win_sslv2.pp
+++ site/example/manifests/greg_demo_win_sslv2.pp
@@ -6,7 +6,9 @@ class example::greg_demo_win_sslv2 {
     purge_values => true,
   }
 
-  registry_key { 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server\Enabled':
+  registry_key { 'Enabled':
+    path       => 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server',
+    ensure     => present,
     type       => dword,
     data       => '0x1',
   }

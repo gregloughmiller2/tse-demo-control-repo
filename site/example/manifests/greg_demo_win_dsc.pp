caf46c2a44b1a73180062d0dcf1b82aa7cd261a8
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-26 14:23:54 -0500

    add dsc for Defender config

--- site/example/manifests/greg_demo_win_dsc.pp
+++ site/example/manifests/greg_demo_win_dsc.pp
@@ -7,7 +7,7 @@ class example::greg_demo_win_dsc {
 
 
 dsc_xmppreference { 'Demo-Defender':
-   dsc_name             => 'Demo-Defender'
+ ,  dsc_name             => 'Demo-Defender'
    dsc_scanscheduleday  => 'Everyday',
    dsc_scanparameters   => 'QuickScan',
    dsc_scanscheduletime => '660',

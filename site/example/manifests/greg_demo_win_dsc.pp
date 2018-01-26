5a4ad9aa695c59b190ffe2520df057738a8fcfe9
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-26 14:28:05 -0500

    add dsc for Defender config

--- site/example/manifests/greg_demo_win_dsc.pp
+++ site/example/manifests/greg_demo_win_dsc.pp
@@ -4,7 +4,6 @@ dsc_xmppreference { 'Demo-Defender':
    dsc_name             => 'Demo-Defender',
    dsc_scanscheduleday  => 'Everyday',
    dsc_scanparameters   => 'QuickScan',
-   dsc_scanscheduletime => 660,
    dsc_disablerealtimemonitoring  => 'True',
 }
 

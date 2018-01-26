2b29f022aa741288d0df4dbaece2fa62201fd606
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-26 08:43:26 -0500

    ssl keys

---
+++ site/example/manifests/greg_demo_win_sslv2.pp
@@ -0,0 +1,14 @@
+class example::greg_demo_win_sslv2 {
+
+  # CUSTOM SSL REG KEYS
+  registry_key { 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\Server':
+    ensure       => present,
+    purge_values => true,
+  }
+
+  registry_key { 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\Server\Enabled':
+    type => dword,
+    data => '0x1',
+  }
+
+}

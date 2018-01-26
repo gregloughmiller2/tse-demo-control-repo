cd5f24bb0a2eec48aa8bb6b5732497e94cf7e063
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-26 09:00:05 -0500

    ssl keys

--- site/example/manifests/greg_demo_win_sslv2.pp
+++ site/example/manifests/greg_demo_win_sslv2.pp
@@ -6,9 +6,9 @@ class example::greg_demo_win_sslv2 {
     purge_values => true,
   }
 
-#  registry_key { 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server\Enabled':
-#    type => dword,
-#    data => '0x1',
-#  }
+  registry_key { 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server\Enabled':
+    type => dword,
+    data => '0x1',
+  }
 
 }

25be9cda7c9c6a9cd7d7c697d2a85eb66878a2af
gregloughmiller2 <gloughmiller@yahoo.com>
2018-01-30 09:54:33 -0500

    add new wsus server ip

--- site/example/manifests/greg_demo_wsus_client.pp
+++ site/example/manifests/greg_demo_wsus_client.pp
@@ -1,7 +1,7 @@
 class example::greg_demo_wsus_client {
 
 class { '::wsus_client':
-    server_url                     => 'http://10.2.1.17:8530',
+    server_url                     => 'http://10.2.1.22:8530',
     auto_update_option             => 'AutoInstall',
     detection_frequency_hours      => '1',
     enable_status_server           => true,
